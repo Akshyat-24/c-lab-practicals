cec9e055ef6d2189c11aeda171fa6c48b01aae04
Akshyat kumar <akshyatkumar2024@gmail.com>
2026-02-17 11:59:37 +0530

    Array-1.1

--- Array-1.c
+++ Array-1.c
@@ -8,20 +8,23 @@ int main(int argc, char const *argv[])
 
     for (int i = 0; i < n; i++)
     {
-scanf("%d" , &arr[i]); 
+    scanf("%d" , &arr[i]); 
     }
 
     // for (int i = 0 ; i <n;i++){
     // printf("%d " , arr[i]);
     // }
+    
     printf("Enter Position : ");
     scanf("%d", &pos);
+    
     // for (int i = 0; i < n; i++)
     // {
     //     if(pos == i){
     //         printf("%d" , arr[i-1]);
     //     }
     // }
+
     printf("%d" , arr[pos]);
     return 0;
 }
